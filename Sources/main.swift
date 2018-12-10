381f12749e5179b94400390e8e5903d99d31d99b
Ron Pinz <ron.pinz@gmail.com>
2018-12-10 09:37:58 -0500

    silence mutability warnings

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,7 +10,7 @@ let status = Application.run { app in
     let listView: ListView
     do {
         let store = ListStore(.string, .string, .boolean)
-        var i = TreeIter()
+        let i = TreeIter()
         store.append(asNextRow: i, "The Principle of Reason", "Martin Heidegger", false)
         store.append(asNextRow: i, "The Art of Computer Programming", "Donald E. Knuth", true)
         store.append(asNextRow: i, "Structure and Interpretation of Computer Programs", "Harold Abelson and Gerald J. Sussman", false)
@@ -20,7 +20,7 @@ let status = Application.run { app in
             ("Title",  "text", CellRendererText()),
             ("Lent", "active", CellRendererToggle())].enumerated().map {
                 (i: Int, c: (title: String, kind: PropertyName, renderer: CellRenderer)) -> TreeViewColumn in
-                var column = TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
+                let column = TreeViewColumn(i, title: c.title, renderer: c.renderer, attribute: c.kind)
                 return column
         }
         listView.append(columns)
