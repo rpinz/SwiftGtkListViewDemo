8b2eb5e6a294d65aa29150b92208f03be13c3efa
Ron Pinz <ron.pinz@gmail.com>
2018-12-11 07:04:59 -0500

    add swift 5 manifest

---
+++ Package@swift-5.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GtkListViewDemo",
+    products: [
+        .executable(
+            name: "GtkListViewDemo",
+            targets: ["GtkListViewDemo"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/rpinz/SwiftGtk",
+            .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "GtkListViewDemo",
+            dependencies: ["Gtk"],
+            path: "Sources"),
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
